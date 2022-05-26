90e9686e729273d22164721a55c03c071e967937
İbrahim Ekmen <44057861+ibrahimekmen@users.noreply.github.com>
2022-05-26 15:36:34 +0300

    Update server.php

--- server.php
+++ server.php
@@ -6,7 +6,7 @@
     $email    = "";
     $errors = array(); 
 
-    $db = mysqli_connect('localhost', 'root', 'Z@h1rM0n3y', 'heroku_37c37043f7fc84a');
+    $db = mysqli_connect('eu-cdbr-west-02.cleardb.net', 'bc32387609d527', '6870312e', 'heroku_37c37043f7fc84a');
 
     if (isset($_POST['reg_user'])) {
         $username = mysqli_real_escape_string($db, $_POST['username']);
